bf7b9e615b7080b26715ec0f5de1f58590e4ccb8
Matheus Fofonka <matheus.fofonka@gmail.com>
2021-04-04 06:25:07 -0300

    Metodo Listar e Incluir da model criados, Classe Retorno e o Transfer Object

--- DBCadastro/DBPessoas.cs
+++ DBCadastro/DBPessoas.cs
@@ -12,7 +12,7 @@ namespace DBCadastro
         MySqlDataAdapter dataAdapter;
         MySqlDataReader dataReader;
 
-        public Retorno<List<ToPessoas>> Listar (ToPessoas toPessoas)
+        public Retorno<List<ToPessoas>> Listar(ToPessoas toPessoas)
         {
             string strCommand = "SELECT ID, NOME, ENDERECO, TELEFONE FROM CADASTROS.PESSOAS ";
 
@@ -78,8 +78,63 @@ namespace DBCadastro
             {
                 conexao.Close();
             }
+
+
         }
 
+        public Retorno<Int32> Incluir(ToPessoas toPessoas)
+        {
+            string strCommand = "INSERT INTO CADASTROS.PESSOAS (ID, NOME, ENDERECO, TELEFONE) ";
 
+            (bool where, string cmd) tID = (toPessoas.Id != null) ? (true, " @ID ,") : (false, "");
+            (bool where, string cmd) tNome = (toPessoas.Nome != null) ? (true, " @NOME ,") : (false, "");
+            (bool where, string cmd) tEndereco = (toPessoas.Endereco != null) ? (true, " @ENDERECO ,") : (false, "");
+            (bool where, string cmd) tTelefone = (toPessoas.Telefone != null) ? (true, " @TELEFONE ,") : (false, "");
+
+            strCommand += (tID.where || tNome.where || tEndereco.where || tTelefone.where) ? "VALUES ( " + tID.cmd + tNome.cmd + tEndereco.cmd + tTelefone.cmd : "";
+
+
+            try
+            {
+                conexao = new("Server = localhost; Database = Cadastros; Uid = root; Pwd = 12354");
+                comando = new(strCommand, conexao);
+
+                if (tID.where)
+                {
+                    comando.Parameters.AddWithValue("@ID", toPessoas.Id);
+                }
+                if (tNome.where)
+                {
+                    comando.Parameters.AddWithValue("@NOME", toPessoas.Nome);
+                }
+                if (tID.where)
+                {
+                    comando.Parameters.AddWithValue("@ENDERECO", toPessoas.Endereco);
+                }
+                if (tID.where)
+                {
+                    comando.Parameters.AddWithValue("@TELEFONE", toPessoas.Telefone);
+                }
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+
+                Retorno<int> ret = new();
+                return ret.RetornarSucesso(1);
+
+            }
+            catch (Exception Ex)
+            {
+                Retorno<int> ret = new();
+                return ret.RetornarFalha(Ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+
+      
     }
 }
